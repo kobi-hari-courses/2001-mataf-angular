b931d74ca7a7ec8bba19cb62028a1a6e3c425eea
Kobi Hari <kobi2294@yahoo.com>
2020-02-24 15:33:15 +0200

    added masking directive

--- Day 06/fun-with-directives/src/app/directives/no-space.directive.ts
+++ Day 06/fun-with-directives/src/app/directives/no-space.directive.ts
@@ -1,10 +1,41 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appNoSpace]'
 })
-export class NoSpaceDirective {
+export class NoSpaceDirective implements OnInit {
+  private lastValidValue: string;
 
-  constructor() { }
+  isValid(value: string):boolean {
+    return !value.includes(' ');
+  }
+
+  // on the host write: (input)="onInput($event)"
+  @HostListener('input', ['$event'])
+  onInput(data: InputEvent) {
+    let elem = <any>data.srcElement;
+    console.log(data)
+
+    this.validate(data.srcElement);
+  }
+
+  validate(elem: any) {
+    let val = elem.value;
+
+    if (this.isValid(val)) {
+      this.lastValidValue = val;
+    } else {
+      elem.value = this.lastValidValue;
+    }
+  }
+
+  constructor(
+    private elem: ElementRef
+  ) { }
+
+  ngOnInit(): void {
+    this.lastValidValue = '';
+    this.validate(this.elem.nativeElement);
+  }
 
 }
